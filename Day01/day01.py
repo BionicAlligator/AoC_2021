ee7e6ee41b12799e6abf82940b5fcc4374e7494f
Andrew Gillard <andrew.gillard@gmail.com>
2023-01-04 17:25:04 +0000

    Day 1 Part 2 complete

--- Day01/day01.py
+++ Day01/day01.py
@@ -18,7 +18,18 @@ def part1():
     return depth_increases
 
 def part2():
-    return
+    depths = read_input()
+
+    depth_increases = 0
+    previous_window = float('inf')
+
+    for reading_num in range(0, len(depths) - 2):
+        window = sum(depths[reading_num:reading_num+3])
+        # print(f"Comparing {window} with {previous_window}")
+        depth_increases += 1 if window > previous_window else 0
+        previous_window = window
+
+    return depth_increases
 
 
 if TESTING:
