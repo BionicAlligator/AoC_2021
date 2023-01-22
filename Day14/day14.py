55056727c340bb173b3a181107115128558018e8
Andrew Gillard <andrew.gillard@gmail.com>
2023-01-22 20:07:11 +0000

    Day 14 Part 2 complete but there may be a more efficient/faster solution

--- Day14/day14.py
+++ Day14/day14.py
@@ -9,7 +9,7 @@ def read_input():
     insertion_rules = {}
 
     for rule in raw_insertion_rules:
-        insertion_rules.update({(rule[0], rule[1]): rule[6]})
+        insertion_rules.update({rule[0:2]: rule[6]})
 
     return template, insertion_rules
 
@@ -19,7 +19,8 @@ def apply_rules(polymer, insertion_rules):
 
     for index in range(len(polymer) - 1):
         new_polymer += polymer[index]
-        pair = (polymer[index], polymer[index + 1])
+
+        pair = polymer[index:index + 2]
 
         if pair in insertion_rules:
             new_polymer += insertion_rules[pair]
@@ -28,35 +29,88 @@ def apply_rules(polymer, insertion_rules):
 
     return new_polymer
 
+def polymerize(template, insertion_rules, steps):
+    polymer = template
 
-def score_polymer(polymer):
-    char_counts = []
+    for step in range(steps):
+        # print(f"Polymerization {step = }")
+        polymer = apply_rules(polymer, insertion_rules)
+
+    return polymer
+
+
+def count_chars(polymer):
+    char_counts = {}
 
     for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
         count = polymer.count(char)
 
         if count > 0:
-            char_counts.append(count)
+            char_counts.update({char: count})
 
-    print(f"{char_counts = }, Max = {max(char_counts)}, Min = {min(char_counts)}")
+    return char_counts
 
-    return max(char_counts) - min(char_counts)
+
+def score_polymer(char_counts):
+    return max(char_counts.values()) - min(char_counts.values())
+
+
+def expand_rules(insertion_rules):
+    expanded_rules = {}
+    rule_char_counts = {}
+
+    for pair, insertion in insertion_rules.items():
+        expanded_insertion = polymerize(pair, insertion_rules, 20)
+        char_counts = count_chars(expanded_insertion[1:len(expanded_insertion) - 1])
+
+        #Don't include the starting and ending characters - we just want the part to be inserted
+        expanded_rules.update({pair: expanded_insertion[1:len(expanded_insertion) - 1]})
+        rule_char_counts.update({pair: char_counts})
+
+    return expanded_rules, rule_char_counts
+
+
+def score_polymer_lookahead(polymer, rule_char_counts):
+    polymer_char_counts = count_chars(polymer)
+
+    for index in range(len(polymer) - 1):
+        pair = polymer[index:index + 2]
+
+        for char in rule_char_counts[pair]:
+            polymer_char_counts[char] += rule_char_counts[pair][char]
+
+    print(f"{polymer_char_counts = }")
+
+    return score_polymer(polymer_char_counts)
+
 
 def part1():
     polymer, insertion_rules = read_input()
 
     print(f"{polymer = }, {insertion_rules = }")
 
-    for step in range(10):
-        polymer = apply_rules(polymer, insertion_rules)
-
+    polymer = polymerize(polymer, insertion_rules, 10)
     print(f"{polymer = }")
 
-    return score_polymer(polymer)
+    char_counts = count_chars(polymer)
+    score = score_polymer(char_counts)
+
+    return score
 
 
 def part2():
-    return
+    polymer, insertion_rules = read_input()
+
+    print(f"{polymer = }, {insertion_rules = }")
+
+    expanded_rules, rule_char_counts = expand_rules(insertion_rules)
+    print(f"{expanded_rules = }, {rule_char_counts = }")
+
+    polymer = polymerize(polymer, expanded_rules, 1)
+
+    score = score_polymer_lookahead(polymer, rule_char_counts)
+
+    return score
 
 
 if TESTING:
