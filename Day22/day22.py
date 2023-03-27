4a08d4bfd849a9a3008cbd77cda26559dd978f87
Andrew Gillard <andrew.gillard@gmail.com>
2023-03-27 21:36:40 +0100

    Day 22 Part 2 before big change

--- Day22/day22.py
+++ Day22/day22.py
@@ -1,6 +1,6 @@
 import re
 
-TESTING = "False"
+TESTING = "NormalA"
 OUTPUT_TO_CONSOLE = True
 
 INIT_RANGE = (-50, 50)
@@ -51,12 +51,38 @@ def intersects_3d(x_range1, y_range1, z_range1, x_range2, y_range2, z_range2):
                    intersects(z_range1, z_range2) else False
 
 
+def intersection(x_range1, y_range1, z_range1, x_range2, y_range2, z_range2):
+    x_intersect = (max(x_range1[0], x_range2[0]), min(x_range1[1], x_range2[1]))
+    y_intersect = (max(y_range1[0], y_range2[0]), min(y_range1[1], y_range2[1]))
+    z_intersect = (max(z_range1[0], z_range2[0]), min(z_range1[1], z_range2[1]))
+    return x_intersect, y_intersect, z_intersect
+
+
 def intersects_init_area(x_range, y_range, z_range):
     return True if intersects_3d(INIT_RANGE, INIT_RANGE, INIT_RANGE,
                                  x_range, y_range, z_range) else False
 
+
+def contains(range1, range2):
+    return True if (range2[0] <= range1[0] <= range2[1]) and \
+                   (range2[0] <= range1[1] <= range2[1]) else False
+
+
+def contains_3d(x_range1, y_range1, z_range1, x_range2, y_range2, z_range2):
+    return True if contains(x_range1, x_range2) and \
+                   contains(y_range1, y_range2) and \
+                   contains(z_range1, z_range2) else False
+
+
 def constrain(value):
     return max(min(value, INIT_RANGE[1]), INIT_RANGE[0])
+
+
+def volume(x_range, y_range, z_range):
+    x = x_range[1] - x_range[0] + 1
+    y = y_range[1] - y_range[0] + 1
+    z = z_range[1] - z_range[0] + 1
+    return x * y * z
 
 
 # Ideas:
@@ -68,9 +94,7 @@ def constrain(value):
 # 2. Determine intersections
 #
 # 3. Create a set of all "on" coords
-#   This works for -50..50 but not for -100000..100000
-#   Work out max and min values for each axis to properly evaluate the scale
-#   Test creation of set of full range of coords
+#   This works for -50..50 (part 1) but not for -100000..100000 (part 2)
 def part1(filename):
     reboot_steps = read_input(filename)
     log(f"{reboot_steps = }")
@@ -90,11 +114,159 @@ def part1(filename):
     return len(on)
 
 
+# Start at end and work toward beginning:
+# For each step A, compare against every earlier step B
+#   If A contains B, delete B (doesn't matter whether A and B are ON or OFF steps)
+#   If A is OFF and does not intersect with any B, delete it
+def reduce_reboot_steps(reboot_steps):
+    new_reboot_steps = []
+
+    while reboot_steps:
+        on_off, x_range1, y_range1, z_range1 = reboot_steps.pop()
+        new_reboot_steps.append((on_off, x_range1, y_range1, z_range1))
+
+        # Only if this is an OFF step will we check for intersections to see if this step
+        # has any effect. By setting this to True for ON steps, we skip further checking.
+        off_intersection = (on_off == "on")
+        step_num = 0
+
+        while step_num < len(reboot_steps):
+            _, x_range2, y_range2, z_range2 = reboot_steps[step_num]
+
+            if contains_3d(x_range2, y_range2, z_range2, x_range1, y_range1, z_range1):
+                reboot_steps.pop(step_num)
+                step_num -= 1
+
+            if not off_intersection and intersects_3d(x_range1, y_range1, z_range1, x_range2, y_range2, z_range2):
+                off_intersection = True
+
+            step_num += 1
+
+        if not off_intersection:
+            new_reboot_steps.pop()
+
+    log(f"{new_reboot_steps = }")
+
+    reboot_steps = list(reversed(new_reboot_steps))
+    log(f"{reboot_steps = }")
+
+    return reboot_steps
+
+
+# Part 2
+# Start at beginning and work toward end:
+# For each step A, if A is ON:
+#   Compare A against every later step B (regardless ON or OFF)
+#     If A and B intersect:
+#       Add intersection to "subtraction" list
+#
+#   For each area X in subtraction list:
+#     Compare against every later area Y in the subtraction list
+#       If X intersects Y:
+#         Add the intersection to the addition list
+#
+#   Start with volume of A
+#     For each range in the subtraction list, subtract the volume of the range
+#     For each range in the addition list, add the volume of the range
+def part2(filename):
+    reboot_steps = read_input(filename)
+    log(f"{reboot_steps = }")
+
+    reboot_steps = reduce_reboot_steps(reboot_steps)
+
+    total_on = 0
+
+    for step_num, (on_off, x_range1, y_range1, z_range1) in enumerate(reboot_steps):
+        if on_off == "on":
+            subtractions = []
+            additions = []
+            # subtractions2 = []
+            # additions2 = []
+            # subtractions3 = []
+            # additions3 = []
+
+            #If this is the last step, just add the entire range
+            if step_num != len(reboot_steps) - 1:
+                for _, x_range2, y_range2, z_range2 in reboot_steps[step_num + 1:]:
+                    if intersects_3d(x_range1, y_range1, z_range1, x_range2, y_range2, z_range2):
+                        subtractions.append(intersection(x_range1, y_range1, z_range1, x_range2, y_range2, z_range2))
+
+
+            for area_num, (x_area1, y_area1, z_area1) in enumerate(subtractions):
+                if area_num < (len(subtractions) - 1):
+                    for x_area2, y_area2, z_area2 in subtractions[area_num + 1:]:
+                        if contains_3d(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2):
+                            # Delete area1
+
+                        if contains_3d(x_area2, y_area2, z_area2, x_area1, y_area1, z_area1):
+                            # Delete area2
+
+                        if intersects_3d(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2):
+                            additions.append(intersection(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2))
+
+            # for area_num, (x_area1, y_area1, z_area1) in enumerate(subtractions):
+            #     if area_num < (len(subtractions) - 1):
+            #         for x_area2, y_area2, z_area2 in subtractions[area_num + 1:]:
+            #             if intersects_3d(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2):
+            #                 additions.append(intersection(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2))
+            #
+            # for area_num, (x_area1, y_area1, z_area1) in enumerate(additions):
+            #     if area_num < (len(additions) - 1):
+            #         for x_area2, y_area2, z_area2 in additions[area_num + 1:]:
+            #             if intersects_3d(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2):
+            #                 subtractions2.append(intersection(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2))
+            #
+            # for area_num, (x_area1, y_area1, z_area1) in enumerate(subtractions2):
+            #     if area_num < (len(subtractions2) - 1):
+            #         for x_area2, y_area2, z_area2 in subtractions2[area_num + 1:]:
+            #             if intersects_3d(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2):
+            #                 additions2.append(intersection(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2))
+            #
+            # for area_num, (x_area1, y_area1, z_area1) in enumerate(additions2):
+            #     if area_num < (len(additions2) - 1):
+            #         for x_area2, y_area2, z_area2 in additions2[area_num + 1:]:
+            #             if intersects_3d(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2):
+            #                 subtractions3.append(intersection(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2))
+            #
+            # for area_num, (x_area1, y_area1, z_area1) in enumerate(subtractions3):
+            #     if area_num < (len(subtractions3) - 1):
+            #         for x_area2, y_area2, z_area2 in subtractions3[area_num + 1:]:
+            #             if intersects_3d(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2):
+            #                 additions3.append(intersection(x_area1, y_area1, z_area1, x_area2, y_area2, z_area2))
+
+            total_on += volume(x_range1, y_range1, z_range1)
+
+            for x_area, y_area, z_area in subtractions:
+                total_on -= volume(x_area, y_area, z_area)
+
+            for x_area, y_area, z_area in additions:
+                total_on += volume(x_area, y_area, z_area)
+
+            # for x_area, y_area, z_area in subtractions2:
+            #     total_on -= volume(x_area, y_area, z_area)
+            #
+            # for x_area, y_area, z_area in additions2:
+            #     total_on += volume(x_area, y_area, z_area)
+            #
+            # for x_area, y_area, z_area in subtractions3:
+            #     total_on -= volume(x_area, y_area, z_area)
+            #
+            # for x_area, y_area, z_area in additions3:
+            #     total_on += volume(x_area, y_area, z_area)
+
+    return total_on
+
+
 if TESTING == "Basic":
     filename = "sampleInput.txt"
-elif TESTING == "Advanced":
+elif TESTING == "Normal":
     filename = "sampleInput1.txt"
+elif TESTING == "NormalA":
+    filename = "sampleInput1a.txt"
+elif TESTING == "Advanced":
+    filename = "sampleInput2.txt"
 else:
     filename = "input.txt"
 
 print("Part 1: ", part1(filename))
+print("Part 2: ", part2(filename))
